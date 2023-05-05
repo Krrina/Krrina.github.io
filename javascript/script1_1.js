011376822d0c99fe3d03685922637892dbae03e2
Krrina <106991553+Krrina@users.noreply.github.com>
2023-05-05 13:56:18 +0300

    Update script1_1.js

--- javascript/script1_1.js
+++ javascript/script1_1.js
@@ -1,6 +1,6 @@
 'use strict'; 
 let age=20
 let gender=2
-if (age>19 && gender == 1 || age>19 && gender == 2){
-    alert('выполнится, если условие истинное (true)')
+if (age>19 & gender == 2){
+    alert('выполнится, если условие истинное (true)');
 }
