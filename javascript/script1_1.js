5646cae1b3fbba56e5178dd797deacc192c6ca3c
Krrina <106991553+Krrina@users.noreply.github.com>
2023-05-05 13:54:43 +0300

    Update script1_1.js

--- javascript/script1_1.js
+++ javascript/script1_1.js
@@ -2,5 +2,5 @@
 let age=20
 let gender=2
 if (age>19 && gender == 1 || age>19 && gender == 2){
-    console.log('выполнится, если условие истинное (true)')
+    alert('выполнится, если условие истинное (true)')
 }
