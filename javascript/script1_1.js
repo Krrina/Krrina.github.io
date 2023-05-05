2e9c2c398d03d426f0eef19b61d2745ffc2ef860
Krrina <106991553+Krrina@users.noreply.github.com>
2023-05-05 14:08:15 +0300

    Update script1_1.js

--- javascript/script1_1.js
+++ javascript/script1_1.js
@@ -1,9 +1,10 @@
-'use strict'; 
-let age=20
-let gender=2
-let phrase = 'Друг'
-if (phrase === 'Друг') {
-    console.log('Привет')
-} else {
-    console.log('Я тебя не знаю')
-}
+//'use strict'; 
+//let age=20
+//let gender=2
+//let phrase = 'Друг'
+//if (phrase === 'Друг') {
+   // console.log('Привет')
+//} else {
+   // console.log('Я тебя не знаю')
+//}
+console.log('Я тебя не знаю')
