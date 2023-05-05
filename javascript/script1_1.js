abc40727ff67113b7d52d02ac9080f3fcaf6100f
Krrina <106991553+Krrina@users.noreply.github.com>
2023-05-05 13:52:44 +0300

    Update script1_1.js

--- javascript/script1_1.js
+++ javascript/script1_1.js
@@ -1,1 +1,6 @@
-alert( 'Привет, мир!' );
+'use strict'; 
+let age=20
+let gender=2
+if (age>19 && gender == 1 || age>19 && gender == 2){
+    console.log('выполнится, если условие истинное (true)')
+}
