27fbf33083b8f9e350251cac375be2e0880c909b
Krrina <106991553+Krrina@users.noreply.github.com>
2023-06-02 19:41:06 +0300

    Update lab2.js

--- javascript/lab2.js
+++ javascript/lab2.js
@@ -26,3 +26,35 @@ function pow(x, n) {
       return BigInt(n) * factorial(n - 1);
     }
   }
+
+  function fib(n) {
+    if (n === 0) {
+      return BigInt(0);
+    } else if (n === 1) {
+      return BigInt(1);
+    } else {
+      let a = BigInt(0);
+      let b = BigInt(1);
+      let c;
+      for (let i = 2; i <= n; i++) {
+        c = a + b;
+        a = b;
+        b = c;
+      }
+      return c;
+    }
+  }
+
+  function compare(x) {
+    return function(y) {
+      if(y > x) {
+        return true;
+      } else if(y < x) {
+        return false;
+      } else {
+        return null;
+      }
+    }
+  }
+
+  
