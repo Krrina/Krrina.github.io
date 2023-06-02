2f75924efde46f4e738cda9f3c7a9ed83dbf1530
Krrina <106991553+Krrina@users.noreply.github.com>
2023-06-03 00:35:11 +0300

    Create lab4_1.js

---
+++ lab4_1.js
@@ -0,0 +1,43 @@
+"use strict";
+let book1 = {};
+book1.title = "The Bazaar of Bad Dreams";
+book1.pubYear = 2016;
+book1.price = 15.99;
+
+let book2 = {
+  title: "Pet Sematary",
+  pubYear: 1983,
+  price: 20.99
+};
+
+let book3={}
+
+for (let prop in book1) {
+  console.log(`${prop}: ${book1[prop]}`);
+}
+
+for (let prop in book2) {
+  console.log(`${prop}: ${book2[prop]}`);
+}
+
+book1.show = function() {
+  console.log(`Title: ${this.title}, Price: ${this.price}`);
+}
+
+book2.show = function() {
+  console.log(`Title: ${this.title}, Price: ${this.price}`);
+}
+
+book1.show();
+book2.show();
+
+function isEmpty(obj) {
+  for (let prop in obj) {
+    return false;
+  }
+  return true;
+}
+
+console.log(isEmpty(book1));
+console.log(isEmpty(book2));
+console.log(isEmpty(book3));
