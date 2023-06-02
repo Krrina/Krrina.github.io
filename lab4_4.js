f94dc63312132ce7fb676f9b16ea645ef8badbb2
Krrina <106991553+Krrina@users.noreply.github.com>
2023-06-03 00:58:23 +0300

    Create lab4_4.js

---
+++ lab4_4.js
@@ -0,0 +1,26 @@
+"use strict"; 
+let obj = {
+    className: 'open menu',
+
+    addClass(cls) {
+      let classes = this.className.split(' ');
+      if (!classes.includes(cls)) {
+        classes.push(cls);
+        this.className = classes.join(' ');
+      }
+      return this;
+    },
+  
+    removeClass(cls) {
+      let classes = this.className.split(' ');
+      let index = classes.indexOf(cls);
+      if (index !== -1) {
+        classes.splice(index, 1);
+        this.className = classes.join(' ');
+      }
+      return this;
+    }
+};
+ console.log( obj.addClass('new')); 
+ console.log(obj.removeClass('open')); 
+ console.log(obj.removeClass('menu'));
