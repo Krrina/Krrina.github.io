ce99346c165e13ff4772d2610a9c3d5c6c3d7d64
Krrina <106991553+Krrina@users.noreply.github.com>
2023-06-05 19:07:12 +0300

    Update lab2.js

--- javascript/lab2.js
+++ javascript/lab2.js
@@ -1,5 +1,12 @@
 "use strict";
 
+/**
+ * Функция, возвращающая x в степени n (n - целое число)
+ * @param {number} x - запрашиваемое числ, котоое возводится в степень n
+ * @param {number} n - степень, в которую возводится x
+ * @returns {number} - значение x, вовзведенное в степень 
+ */
+
 function pow(x, n) {
     /*return x**n;*/
     if (n==0){ return 1
@@ -11,6 +18,12 @@ function pow(x, n) {
     }else return 1 / pow(x, -n);
   }
 
+/**
+ * вычисляет сумму чисел от 1 до n включительно
+ * @param {number} n - число, до которого необходимо рассчитать сумму
+ * @returns {number} - сумму всех чисел от n до 1 
+ */
+
   function sumTo(n) {
     if (n == 1) {
       return 1;
@@ -19,6 +32,12 @@ function pow(x, n) {
     }
   }
 
+/**
+ * возвращает факториал числа n!
+ * @param {number} n - число, для которого вычисляется факториал 
+ * @returns {BigInt} - Факториал заданного числа в виде BigInt
+ */
+
   function factorial(n) {
     if (n === 0 || n === 1) {
       return BigInt(1);
@@ -26,6 +45,12 @@ function pow(x, n) {
       return BigInt(n) * factorial(n - 1);
     }
   }
+
+/**
+ * возвращает n-е число Фибоначчи
+ * @param {number} n - индекс желаемого числа Фибоначчи
+ * @returns  {BigInt} - n-е число Фибоначчи в виде BigInt
+ */
 
   function fib(n) {
     if (n === 0) {
@@ -45,6 +70,12 @@ function pow(x, n) {
     }
   }
 
+/**
+ * принимает целочисленное значение x и возвращает анонимную функцию
+ * @param {number} x - значение, с которым нужно сравнить
+ * @returns {function} -  функция, которая принимает входное значение и возвращает логическое значение
+ */
+
   function compare(x) {
     return function(y) {
       if(y > x) {
@@ -56,6 +87,12 @@ function pow(x, n) {
       }
     }
   }
+
+/**
+ * возвращает сумму всех своих аргументов
+ * @param  {...number} args - значения дя суммирования
+ * @returns {number} -  общая сумма всех аргументов
+ */
 
   function sum(...args) {
     return args.reduce((total, current) => total + current, 0);
